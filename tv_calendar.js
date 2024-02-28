71f6c8bcdfa355f765b6152a69b610811dade61e
sikrin <atboom33w@gmail.com>
2024-02-28 18:48:06 -0500

    try catch

--- tv_calendar.js
+++ tv_calendar.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TradingView Economic Calendar Grabber
 // @namespace    http://tampermonkey.net/
-// @version      1.1.4
+// @version      1.1.5
 // @description  Get interesting events from economic calendar
 // @author       You
 // @match        https://www.tradingview.com/chart/*
@@ -16,8 +16,12 @@ while(i <= 10){
         console.log("Waiting on page load...");
     }, seconds * 1000);
     i++;
-    let loaded = document.querySelector("div[class$='widgetbar-widget-reuters_calendar']");
-    console.log(loaded.textContent.slice(0, 10));
+    try {
+        let loaded = document.querySelector("div[class$='widgetbar-widget-reuters_calendar']");
+        console.log(loaded.textContent.slice(0, 10));
+    } catch(err) {
+        console.log(err)
+    }
     if (loaded) {
         i = 100;
     };
