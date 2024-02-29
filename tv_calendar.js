d6e14c56773b2d00d7d7ec1f0787c184549140a6
sikrin <atboom33w@gmail.com>
2024-02-28 19:52:48 -0500

    move functions out

--- tv_calendar.js
+++ tv_calendar.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TradingView Economic Calendar Grabber
 // @namespace    http://tampermonkey.net/
-// @version      1.1.13
+// @version      1.1.14
 // @description  Get interesting events from economic calendar
 // @author       You
 // @match        https://www.tradingview.com/chart/*
@@ -13,70 +13,70 @@
 
 waitForKeyElements ("div[class$='widgetbar-widget-reuters_calendar']", mainish)
 
+function hasClass(classes, search_str) {
+    let re = new RegExp(search_str);
+    for (let i = 0; i < classes.length; i++) {
+        if (re.test(classes[i])) {
+            return true;
+        };
+    };
+    return false;
+};
+
+function addCheckBox(node, name, value) {
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.name = name;
+    checkbox.value = value;
+    checkbox.id = "tv_cal_checkbox";
+    node.appendChild(checkbox);
+};
+
+function addCheckBoxes(calender_entries) {
+    let found_today = null
+    for (let i = 0; i < calender_entries.length; i++) {
+        let node = calender_entries[i]
+        if (hasClass(node.classList, "spinnerContainer") || hasClass(node.classList, "timelineDivider")) {
+            continue;
+        } else if (hasClass(node.classList, "separator")) {
+            let element_date = new Date (node.querySelector("div[class^='innerWrapper']").querySelector("span").textContent);
+            let current_date = new Date();
+
+            if (element_date.getMonth() == current_date.getMonth() && element_date.getDate() < current_date.getDate()) {
+                continue;
+            } else if (element_date.getMonth() == current_date.getMonth() && element_date.getDate() == current_date.getDate()) {
+                found_today = true
+                continue;
+            } else {
+                break;
+            };
+        } else {
+            if (Boolean(found_today)) {
+                let title = node.querySelector("div[class^='title']").querySelector("span").textContent;
+                let time = node.querySelector("div[class^='time']").querySelector("span").textContent;
+                let icons = node.querySelector("div[class^='icons']");
+                addCheckBox(icons, title, time);
+            };
+        };
+    };
+};
+
+function addCopyButton(node) {
+    let copy_button = document.createElement("button");
+    copy_button.textContent = "Copy Events";
+    copy_button.addEventListener('click', copyButtonClick);
+    node.prepend(copy_button)
+};
+
+function copyButtonClick() {
+    let checkboxes = document.querySelectorAll("input[type='checkbox']:checked[id^='tv_cal_checkbox']")
+    let combined = []
+    checkboxes.forEach(function (node) {combined.push(node.name + "|" + node.value)});
+    navigator.clipboard.writeText(combined);
+};
+
 function mainish(node) {
     'use strict';
-
-    function hasClass(classes, search_str) {
-        let re = new RegExp(search_str);
-        for (let i = 0; i < classes.length; i++) {
-            if (re.test(classes[i])) {
-                return true;
-            };
-        };
-        return false;
-    };
-
-    function addCheckBox(node, name, value) {
-        var checkbox = document.createElement('input');
-        checkbox.type = "checkbox";
-        checkbox.name = name;
-        checkbox.value = value;
-        checkbox.id = "tv_cal_checkbox";
-        node.appendChild(checkbox);
-    };
-
-    function addCheckBoxes(calender_entries) {
-        let found_today = null
-        for (let i = 0; i < calender_entries.length; i++) {
-            let node = calender_entries[i]
-            if (hasClass(node.classList, "spinnerContainer") || hasClass(node.classList, "timelineDivider")) {
-                continue;
-            } else if (hasClass(node.classList, "separator")) {
-                let element_date = new Date (node.querySelector("div[class^='innerWrapper']").querySelector("span").textContent);
-                let current_date = new Date();
-
-                if (element_date.getMonth() == current_date.getMonth() && element_date.getDate() < current_date.getDate()) {
-                    continue;
-                } else if (element_date.getMonth() == current_date.getMonth() && element_date.getDate() == current_date.getDate()) {
-                    found_today = true
-                    continue;
-                } else {
-                    break;
-                };
-            } else {
-                if (Boolean(found_today)) {
-                    let title = node.querySelector("div[class^='title']").querySelector("span").textContent;
-                    let time = node.querySelector("div[class^='time']").querySelector("span").textContent;
-                    let icons = node.querySelector("div[class^='icons']");
-                    addCheckBox(icons, title, time);
-                };
-            };
-        };
-    };
-
-    function addCopyButton(node) {
-        let copy_button = document.createElement("button");
-        copy_button.textContent = "Copy Events";
-        copy_button.addEventListener('click', copyButtonClick);
-        node.prepend(copy_button)
-    };
-
-    function copyButtonClick() {
-        let checkboxes = document.querySelectorAll("input[type='checkbox']:checked[id^='tv_cal_checkbox']")
-        let combined = []
-        checkboxes.forEach(function (node) {combined.push(node.name + "|" + node.value)});
-        navigator.clipboard.writeText(combined);
-    };
 
     var elem = document.querySelector("div[class$='widgetbar-widget-reuters_calendar']");
     console.log("elem: " + elem)
